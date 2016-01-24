391dbb9ec69206948a8a6358590a47ef29c824f7
MiracleBlue <foreverskybound@gmail.com>
2016-01-24 15:30:52 +1100

    forgot all this stuff

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,5 @@
 import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
+import { setResolver } from 'ember-mocha';
+import chai from 'chai';
 
 setResolver(resolver);
